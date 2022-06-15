05cfe4c1f73befea8e8caa9c3b7fccf83eceae9f
Artem <nimoshka@gmail.com>
2022-06-15 23:30:44 +0300

    Clean code

--- index.js
+++ index.js
@@ -1,81 +1,78 @@
 const gameBoard = (function () {
 
     const _board = [0, 1, 2, 3, 4, 5, 6, 7, 8];
-
+    const _winRows = [[0, 1, 2], [3, 4, 5], [6, 7, 8], [0, 3, 6], [1, 4, 7], [2, 5, 8], [0, 4, 8], [2, 4, 6]];
+    
     function placeMarkOnBoard(mark, cell) {
-
-        if(_board[cell] === +cell){
-
-        _board[cell] = mark;
-        return true;
-
+        if (_board[cell] === cell) {
+            _board[cell] = mark;
+            return true;
         } else {
-
             return false;
         };
     };
 
     function renderBoard() {
-
         const cells = document.querySelectorAll('.cell');
         cells.forEach(element => { _renderCell(element); });
     };
 
     function _renderCell(cell) {
-
-        const cellIndex = +cell.dataset.index;
-        if (typeof board[cellIndex] === 'string') { cell.textContent = board[cellIndex] };
+        const cellIndex = cell.dataset.index;
+        if (typeof _board[cellIndex] === 'string') { cell.textContent = _board[cellIndex] };
     };
 
     function getBoard() {
         return _board;
-    }
+    };
 
-    function findWinRow() {
+    function getWinRows() {
+        return _winRows;
+    };
 
-        if (_board[0] === _board[1] && _board[1] === _board[2]) {
-            _turnWinRed(0, 1, 2)
-        } else if (_board[3] === _board[4] && _board[4] === _board[5]) {
-            _turnWinRed(cellA, cellB, cellC);
-        } else if (_board[6] === _board[7] && _board[7] === _board[8]) {
-            _turnWinRed(6, 7, 8);
-        } else if (_board[0] === _board[3] && _board[6] === _board[5]) {
-            _turnWinRed(0, 3, 6);
-        } else if (_board[1] === _board[4] && _board[4] === _board[7]) {
-            _turnWinRed(1, 4, 7);
-        } else if (_board[2] === _board[5] && _board[5] === _board[8]) {
-            _turnWinRed(2, 5, 8);
-        } else if (_board[0] === _board[4] && _board[4] === _board[8]) {
-            _turnWinRed(0, 4, 8);
-        } else if (_board[2] === _board[4] && _board[4] === _board[6]) {
-            _turnWinRed(2, 4, 6);
+    function findWinRow(board, player) {
+        const winRow = [];
+        console.log(board);
+        _winRows.forEach(row => {
+            if (board[row[0]] === player && board[row[1]] === player && board[row[2]] === player) {
+                row.forEach(cell => { winRow.push(cell) })
+            };
+        });
+        if (winRow.length !== 0) {
+            return winRow
+        } else {
+            return false;
         };
     };
 
-    function _turnWinRed(cellA, cellB, cellC){
-        document.querySelector(`.cell_${cellA}`).style.color = '#ef9f9f';
-        document.querySelector(`.cell_${cellB}`).style.color = '#ef9f9f';
-        document.querySelector(`.cell_${cellC}`).style.color = '#ef9f9f';
+    function checkTie(board) {
+        let availableCells = emptyCells(board);
+        if (availableCells.length === 0){
+            return true;
+        } else {
+            return false;
+        };
+    };
+
+    function turnCellRed(arr) {
+        arr.forEach(cell => {
+            document.querySelector(`.cell_${cell}`).style.color = '#ef9f9f'
+        });
+    };
+
+    function emptyCells(board) {
+        return board.filter(cell => cell !== 'X' && cell !== 'O');
     }
 
     return {
         placeMarkOnBoard,
         renderBoard,
         getBoard,
+        getWinRows,
+        turnCellRed,
         findWinRow,
+        checkTie,
     };
-})();
-
-const playerX = (function () {
-
-    let mode = 'player';
-
-})();
-
-const playerO = (function () {
-
-    let mode = 'player';
-
 })();
 
 const gameFlow = (function () {
@@ -85,40 +82,20 @@ const gameFlow = (function () {
     let currentPlayer = 'X';
 
     function handleMove() {
-
-        const cell = this.dataset.index;
-
-        if(!gameBoard.placeMarkOnBoard(currentPlayer, cell)) return;
+        const cell = +this.dataset.index;
+        if (!gameBoard.placeMarkOnBoard(currentPlayer, cell)) return;
         gameBoard.renderBoard();
-        _checkWin();
+        _checkWinTie();
         _changePlayer();
     };
 
-    function _checkWin() {
-
-        if (wining(gameBoard.getBoard(), playerXMark)) {
-            gameBoard.findWinRow();
-        } else if (wining(gameBoard.getBoard(), playerOMark)) {
-            gameBoard.findWinRow();
-        }
-    };
-
-    function wining(board, player) {
-
-        if (
-            (board[0] == player && board[1] == player && board[2] == player) ||
-            (board[3] == player && board[4] == player && board[5] == player) ||
-            (board[6] == player && board[7] == player && board[8] == player) ||
-            (board[0] == player && board[3] == player && board[6] == player) ||
-            (board[1] == player && board[4] == player && board[7] == player) ||
-            (board[2] == player && board[5] == player && board[8] == player) ||
-            (board[0] == player && board[4] == player && board[8] == player) ||
-            (board[2] == player && board[4] == player && board[6] == player)
-        ) {
-            return true;
-        } else {
-            return false;
-        }
+    function _checkWinTie() {
+        const board = gameBoard.getBoard();
+        if (gameBoard.findWinRow(board, currentPlayer)) {
+            gameBoard.turnCellRed(gameBoard.findWinRow(board, currentPlayer));
+        } else if (gameBoard.checkTie(board)) {
+            gameBoard.turnCellRed([0, 1, 2, 3, 4, 5, 6, 7, 8]);
+        };
     };
 
     function _changePlayer() {
@@ -131,8 +108,8 @@ const gameFlow = (function () {
 
     return {
         handleMove,
-        wining,
     };
+
 })();
 
 // EVENT LISTENERS
